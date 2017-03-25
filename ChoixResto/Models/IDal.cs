6030f03f90b99a3b00b81fd5a818a9b37812b796
dell <chrisebongue@hotmail.fr>
2017-03-25 19:20:28 +0100

    first commit

---
+++ ChoixResto/Models/IDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoixResto.Models
+{
+    public interface IDal : IDisposable
+    {
+        List<Resto> ObtientTousLesRestaurants();
+        void CreerRestaurant(string nom, string telephone);
+     
+    }
+}
